4a2d9eb3320dd68054d2b58a11032d3414acfc41
bezugomonnov <bezugomonnov@netcracker.com>
2024-07-31 16:32:12 +0500

    initial commit

---
+++ fpa/src/main/java/org/robe/fpa/FianancePersonalApplication.java
@@ -0,0 +1,13 @@
+package org.robe.fpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FianancePersonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FianancePersonalApplication.class, args);
+	}
+
+}
---
+++ fpa/src/test/java/org/robe/fpa/FianancePersonalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.robe.fpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FianancePersonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
