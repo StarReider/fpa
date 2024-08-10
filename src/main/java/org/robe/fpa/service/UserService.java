9d0fb43eb9cdd67e21ae7db5c230856d0b2d551b
bezugomonnov <bezugomonnov@netcracker.com>
2024-08-10 21:52:52 +0500

    tune apptune app

--- src/main/java/org/robe/fpa/service/UserService.java
+++ src/main/java/org/robe/fpa/service/UserService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-    @Autowired
+    @Autowired(required = false)
     private UserRepository userRepository;
 
     public List<User> getAllUsers() {
