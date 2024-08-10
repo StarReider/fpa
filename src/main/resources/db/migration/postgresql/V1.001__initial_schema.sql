4934862713c8a002895e0a912b35fa2d4f95869e
bezugomonnov <bezugomonnov@netcracker.com>
2024-08-10 23:23:35 +0500

    fix schema issues

--- src/main/resources/db/migration/postgresql/V1.001__initial_schema.sql
+++ src/main/resources/db/migration/postgresql/V1.001__initial_schema.sql
@@ -1,43 +1,44 @@
-CREATE TABLE Users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TYPE transaction_type AS ENUM('debit', 'credit', 'interest');
+
+CREATE TABLE IF NOT EXISTS Users (
+    user_id SMALLSERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
     email VARCHAR(100) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP --ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE Accounts (
-    account_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    account_name VARCHAR(50) NOT NULL,
-    account_type VARCHAR(20) NOT NULL,
-    balance DECIMAL(10, 2) DEFAULT 0.00,
-    currency_code VARCHAR(10) NOT NULL, -- Foreign key to Currencies
-    interest_rate DECIMAL(5, 2) DEFAULT 0.00, -- Interest rate as a percentage
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES Users(user_id),
-    FOREIGN KEY (currency_code) REFERENCES Currencies(currency_code)
-);
-
-CREATE TABLE Transactions (
-    transaction_id INT AUTO_INCREMENT PRIMARY KEY,
-    account_id INT,
-    amount DECIMAL(10, 2) NOT NULL,
-    transaction_type ENUM('debit', 'credit', 'interest') NOT NULL,
-    transaction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    description VARCHAR(255),
-    is_scheduled BOOLEAN DEFAULT FALSE,
-    scheduled_date TIMESTAMP NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (account_id) REFERENCES Accounts(account_id)
-);
-
-CREATE TABLE Currencies (
+CREATE TABLE IF NOT EXISTS Currencies (
     currency_code VARCHAR(10) PRIMARY KEY,
     currency_name VARCHAR(50) NOT NULL,
     exchange_rate DECIMAL(10, 6) NOT NULL, -- Exchange rate compared to base currency
     base_currency VARCHAR(10) NOT NULL -- Base currency code
+);
+
+CREATE TABLE IF NOT EXISTS Accounts (
+    account_id SMALLSERIAL PRIMARY KEY,
+    user_id INT,
+    account_name VARCHAR(50) NOT NULL,
+    account_type VARCHAR(20) NOT NULL,
+    balance DECIMAL(10, 2) DEFAULT 0.00,
+    currency VARCHAR(10) NOT NULL, -- Foreign key to Currencies
+    interest_rate DECIMAL(5, 2) DEFAULT 0.00, -- Interest rate as a percentage
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP, -- ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (currency) REFERENCES Currencies(currency_code)
+);
+
+CREATE TABLE IF NOT EXISTS Transactions (
+    transaction_id SERIAL PRIMARY KEY,
+    account_id INT,
+    amount DECIMAL(10, 2) NOT NULL,
+    type transaction_type NOT NULL,
+    description VARCHAR(255),
+    is_scheduled BOOLEAN DEFAULT FALSE,
+    scheduled_date TIMESTAMP NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP, --ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (account_id) REFERENCES Accounts(account_id)
 );
